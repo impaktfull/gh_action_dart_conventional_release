a4b8ec39f2dd9110f0624d11b9633a64d2d2c928
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2023-11-26 13:42:34 +0100

    refactor: Switch to official github actions dependencies

--- index.js
+++ index.js
@@ -1,93 +1,80 @@
-const { Toolkit } = require('actions-toolkit')
-const fs = require('fs')
-const yaml = require('js-yaml')
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const semver = require('semver');
+const exec = require('@actions/exec');
 
 // Change working directory if user defined PACKAGEJSON_DIR
 if (process.env.PACKAGEJSON_DIR) {
-  process.env.GITHUB_WORKSPACE = `${process.env.GITHUB_WORKSPACE}/${process.env.PACKAGEJSON_DIR}`
-  process.chdir(process.env.GITHUB_WORKSPACE)
+  process.chdir(`${process.env.GITHUB_WORKSPACE}/${process.env.PACKAGEJSON_DIR}`);
 }
 
 // Helper function to read and parse the pubspec.yaml file
 function getPubspec() {
-  const pubspecContent = fs.readFileSync('pubspec.yaml', 'utf8')
-  return yaml.safeLoad(pubspecContent)
+  const pubspecContent = fs.readFileSync('pubspec.yaml', 'utf8');
+  return yaml.safeLoad(pubspecContent);
 }
+
 // Helper function to write updates to the pubspec.yaml file
 function updatePubspec(version) {
-  let pubspec = getPubspec()
-  pubspec.version = version
-  fs.writeFileSync('pubspec.yaml', yaml.safeDump(pubspec), 'utf8')
+  let pubspec = getPubspec();
+  pubspec.version = version;
+  fs.writeFileSync('pubspec.yaml', yaml.safeDump(pubspec), 'utf8');
 }
 
 // Increment version based on the type (major, minor, patch)
 function incrementVersion(currentVersion, type) {
-  return semver.inc(currentVersion, type)
+  return semver.inc(currentVersion, type);
 }
 
-// Run your GitHub Action!
-Toolkit.run(async tools => {
-  const pubspec = getPubspec()
-  const event = tools.context.payload
-
-  const messages = event.commits.map(commit => commit.message + '\n' + commit.body)
-
-  const commitMessage = 'version bump to'
-  const isVersionBump = messages.map(message => message.toLowerCase().includes(commitMessage)).includes(true)
-  if (isVersionBump) {
-    tools.exit.success('No action necessary!')
-    return
-  }
-
-  let versionType = 'patch'
-  if (messages.map(message => message.includes('BREAKING CHANGE') || message.includes('major')).includes(true)) {
-    versionType = 'major'
-  } else if (messages.map(
-    message => message.toLowerCase().startsWith('feat') || message.toLowerCase().includes('minor')).includes(true)) {
-    versionType = 'minor'
-  }
-
+async function run() {
   try {
-    const currentVersion = pubspec.version.toString()
+    const pubspec = getPubspec();
+    const event = github.context.payload;
 
-    // Set git user
-    await tools.runInWorkspace('git', ['config', 'user.name', '"Dart Conventional Release"'])
-    await tools.runInWorkspace('git', ['config', 'user.email', 'gh_action_dart_conventional_release@users.noreply.github.com"'])
+    const messages = event.commits.map(commit => commit.message + '\n' + commit.body);
 
-    const currentBranch = /refs\/[a-zA-Z]+\/(.*)/.exec(process.env.GITHUB_REF)[1]
-    console.log('currentBranch:', currentBranch)
-
-    console.log('current:', currentVersion, '/', 'version:', versionType)
-    const newVersion = incrementVersion(currentVersion, versionType)
-    console.log('new version:', newVersion)
-    updatePubspec(newVersion)
-
-    // Do it in the current checked out github branch (DETACHED HEAD)
-    // Important for further usage of the pubspec.yaml version
-    await tools.runInWorkspace('git', ['commit', '-a', '-m', `ci: ${commitMessage} ${newVersion}`])
-
-    // Now go to the actual branch to perform the same versioning
-    await tools.runInWorkspace('git', ['checkout', currentBranch])
-
-    try {
-      // To support "actions/checkout@v1"
-      await tools.runInWorkspace('git', ['commit', '-a', '-m', `ci: ${commitMessage} ${newVersion}`])
-    } catch (e) {
-      console.warn('git commit failed because you are using "actions/checkout@v2", ' +
-        'but that doesnt matter because you dont need that git commit, thats only for "actions/checkout@v1"')
+    const commitMessage = 'version bump to';
+    const isVersionBump = messages.map(message => message.toLowerCase().includes(commitMessage)).includes(true);
+    if (isVersionBump) {
+      core.info('No action necessary!');
+      return;
     }
 
-    const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`
+    let versionType = 'patch';
+    if (messages.map(message => message.includes('BREAKING CHANGE') || message.includes('major')).includes(true)) {
+      versionType = 'major';
+    } else if (messages.map(
+      message => message.toLowerCase().startsWith('feat') || message.toLowerCase().includes('minor')).includes(true)) {
+      versionType = 'minor';
+    }
 
-    const tagVersion = `${process.env['INPUT_TAG-PREFIX']}${newVersion}`
-    await tools.runInWorkspace('git', ['tag', tagVersion])
-    await tools.runInWorkspace('git', ['pull', remoteRepo, '--no-edit'])
-    await tools.runInWorkspace('git', ['push', remoteRepo, '--follow-tags', '--no-verify'])
-    await tools.runInWorkspace('git', ['push', remoteRepo, '--tags', '--no-verify'])
-  } catch (e) {
-    tools.log.fatal(e)
-    tools.exit.failure('Failed to bump version')
-    return
+    const currentVersion = pubspec.version.toString();
+    const newVersion = incrementVersion(currentVersion, versionType);
+    updatePubspec(newVersion);
+
+    // Setting up Git
+    await exec.exec('git', ['config', 'user.name', 'Dart Conventional Release']);
+    await exec.exec('git', ['config', 'user.email', 'gh_action_dart_conventional_release@github.com']);
+
+    // Committing changes
+    await exec.exec('git', ['add', 'pubspec.yaml']);
+    await exec.exec('git', ['commit', '-m', `ci: ${commitMessage} ${newVersion}`]);
+
+    // Tagging the commit
+    const tag = `v${newVersion}`;
+    await exec.exec('git', ['tag', tag]);
+
+    // Pushing changes
+    const token = core.getInput('GITHUB_TOKEN', { required: true });
+    const remoteRepo = `https://${token}:x-oauth-basic@github.com/${process.env.GITHUB_REPOSITORY}.git`;
+    await exec.exec('git', ['push', remoteRepo]);
+    await exec.exec('git', ['push', remoteRepo, '--tags']);
+
+  } catch (error) {
+    core.setFailed(`Action failed with error: ${error}`);
   }
-  tools.exit.success('Version bumped!')
-})
+}
+
+run();
