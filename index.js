067ea1df3852fb90b9429f67af4744431cf554fb
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2023-11-30 10:05:22 +0100

    fix: set the oauth2 app for remotegitrepourl

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ async function run() {
     await runInWorkspace('git', ['tag', tag])
 
     // Pushing changes
-    const remoteGitRepoUrl = `https://${githubToken}@github.com/${process.env.GITHUB_REPOSITORY}.git`
+    const remoteGitRepoUrl = `https://oauth2:${githubToken}@github.com/${process.env.GITHUB_REPOSITORY}.git`
     await runInWorkspace('git', ['push', remoteGitRepoUrl])
     await runInWorkspace('git', ['push', remoteGitRepoUrl, '--tags'])
 
