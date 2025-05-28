c90c813a339ffad17eae70d5af995da167cfff4d
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2025-05-28 23:14:46 +0200

    fix: test connection with github

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ async function run() {
         `git@github.com:${github.context.repo.owner}/${github.context.repo.repo}.git`
       ])
       await runInWorkspace('ssh', [
-        '-o',
-        'StrictHostKeyChecking=no',
+        '-o', 'IdentitiesOnly=yes',
+        '-o', 'StrictHostKeyChecking=no',
         '-i',
         deployKeyPath,
         'git@github.com',
